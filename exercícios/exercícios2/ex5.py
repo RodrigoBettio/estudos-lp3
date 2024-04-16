fc17f111e5e0c38554f05e2fef92893f99abd4a8
Rodrigo <rodriguinhobettiojr1@gmail.com>
2024-04-16 20:26:14 -0300

    Exercicios 5 e 6 resolvidos

--- exercícios/exercícios2/ex5.py
+++ exercícios/exercícios2/ex5.py
@@ -1,2 +1,23 @@
 #Verificador de Palíndromos: Solicite ao usuário que digite uma palavra ou frase e 
 #verifique se é um palíndromo (ou seja, pode ser lida de frente para trás e de trás para frente da mesma forma).
+
+def palindromo():
+    palavra = input("Digite uma palavra para verificarmos se é um palíndromo ou não: ")
+    return palavra
+
+def verificadorPalindromo(palavra):
+    palavra = palavra.lower()  
+    if palavra == palavra[::-1]:  
+        return True
+    else:
+        return False
+
+def mostrarPalindromo():
+    palavra = palindromo()
+    if verificadorPalindromo(palavra):
+        print("A palavra/frase é um palíndromo!")
+    else:
+        print("A palavra/frase não é um palíndromo.")
+
+mostrarPalindromo()
+
--- exercícios/exercícios2/ex6.py
+++ exercícios/exercícios2/ex6.py
@@ -1,2 +1,24 @@
 #Conversor de Notas Escolares: Baseado em uma pontuação fornecida pelo usuário (0 a 100), 
 #converta para o sistema de notas A, B, C, D, ou F, seguindo os padrões de conversão comuns.
+
+def conversor_notas(notas):
+    if notas >= 90:
+        return 'A'
+    elif notas >= 80:
+        return 'B'
+    elif notas >= 70:
+        return 'C'
+    elif notas >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+def mostrar_resultado():
+    notas = float(input("Digite a pontuação (0 a 100): "))
+    if notas < 0 or notas > 100:
+        print("Pontuação inválida. Por favor, digite uma pontuação entre 0 e 100.")
+    else:
+        nota = conversor_notas(notas)
+        print(f"A nota correspondente a pontuação {notas} é: {nota}")
+
+mostrar_resultado()
