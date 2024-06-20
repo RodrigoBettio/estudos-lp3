89d29bb657f0d64d2218c4bb09c241376af31141
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-06-20 15:04:21 -0300

    Adicionar Mais Páginas

--- projeto_lp3/projeto_lp3/app.py
+++ projeto_lp3/projeto_lp3/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from validate_docbr import CPF, CNPJ
+
 
 app = Flask(__name__)
 
@@ -6,27 +8,38 @@ app = Flask(__name__)
 
 # / - home page
 @app.route("/")
-
 def home():
     return render_template ("home.html")
 
 
 # /contato - pagina de contato 
 @app.route("/contato")
-
 def contato():
     return render_template ("contato.html")
 
 
 # /produtos - pagina produtos 
 @app.route("/produtos")
-
 def produtos():
 
     lista_produtos = [
-        {"nome": "coquinha bb", "desc": "ruim"},
-        {"nome": "doritos", "desc": "suja a mao"},
-        {"nome": "sneakers", "desc": "meu choc preferido"}
+        {"nome": "Coquinha", "desc": "Suco é melhor"},
+        {"nome": "Doritos", "desc": "Suja a mao"},
+        {"nome": "Sneakers", "desc": "Eca"}
     ]
 
     return render_template ("produtos.html", produtos=lista_produtos)
+
+@app.route("/cnpj")
+def cnpj():
+    cnpj = CNPJ()
+    cnpj_gerado = cnpj.generate(True)  # Gera CNPJ com máscara
+    return render_template("cnpj.html", cnpj = cnpj_gerado)
+
+@app.route("/cpf")
+def cpf():
+    cpf = CPF()
+    cpf_gerado = cpf.generate(True)  # Gera CPF com máscara
+    return render_template("cpf.html", cpf = cpf_gerado)
+
+app.run(debug=True)
